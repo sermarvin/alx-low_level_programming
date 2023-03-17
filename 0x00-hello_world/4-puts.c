0fcee4f1d1df999f552cdc7cdb6ba55d4d64db4b
sermarvin <marv.kariuki@gmail.com>
2023-03-17 05:55:26 +0530

    prints using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+ * main - prints using the function pusts
+ *
+ * Return: Always 0 (Sucess)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");

984cfefca01982cfbfa53ebdebccf14b6d43ab36
sermarvin <marv.kariuki@gmail.com>
2023-03-22 08:22:06 +0530

    Advanced tasks

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 Fibonacci numbers
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	int inc;
+	unsigned long n1 = 0, n2 = 1, n3;
+
+	for (inc = 0; inc < 50; inc++)
+	{
+		n3 = n1 + n2;
+		printf("%lu", n3);
+		n1 = n2;
+		n2 = n3;
+
+		if (inc == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - finds and prints the sum of the even-valued terms
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	unsigned long n1 = 0, n2 = 1, n3 = 0, sum = 0;
+
+	while (n3 <= 4000000)
+	{
+		n3 = n1 + n2;
+		n1 = n2;
+		n2 = n3;
+
+		if ((n1 % 2) == 0)
+			sum += n1;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}

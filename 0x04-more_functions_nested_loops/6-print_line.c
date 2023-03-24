9fc1b7ff9fb7a1250d7ada379c45153c6b68c472
sermarvin <marv.kariuki@gmail.com>
2023-03-24 07:32:08 +0530

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: The number of times the character _ should be printed
+ *
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n >  0)
+	{
+		i = 0;
+		while (i < n)
+		{
+			_putchar(95);
+			i++;
+		}
+		_putchar(10);
+	}
+	else 
+		_putchar(10);
+}

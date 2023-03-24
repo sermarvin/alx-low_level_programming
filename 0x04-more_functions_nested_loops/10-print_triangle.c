cc58c7410d1cd011d4d367e3fe27e6b4776a4577
sermarvin <marv.kariuki@gmail.com>
2023-03-24 08:12:54 +0530

    task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * Print_traingle - prints triangle
+ * @size: size parameter of a triangle
+ * Return: Nothing
+ */
+
+void print_triangle(int size)
+{
+	int b, h;
+
+	if (size > 0)
+	{
+		for (b = 1; b <= size; b++)
+		{
+			for ((h = size - b); h > 0; h--)
+			{
+				_putchar(' ');
+			}
+			for (h = 0; h < b; h++)
+			{
+				_putchar('#');
+			}
+			if (b == size)
+			{
+				continue;
+			}
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}

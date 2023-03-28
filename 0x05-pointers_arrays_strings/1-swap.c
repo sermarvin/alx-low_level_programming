8d7ffc999f8a18d499b53cac045847e24543ddef
sermarvin <marv.kariuki@gmail.com>
2023-03-28 06:14:20 +0530

    task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * void swap_int - swaps the values of two integers
- * a@: first integer
+ * swap_int - swaps the values of two integers
+ * @a: first integer
  * @b: second integer
  *
  * Return: 0;

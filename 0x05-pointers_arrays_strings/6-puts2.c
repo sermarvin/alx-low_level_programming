e0ec1858bf6f9ac21dad6156d168f3ba56e31443
sermarvin <marv.kariuki@gmail.com>
2023-03-28 07:17:06 +0530

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - prints every other character from first followed by new line
+ * @str: string
+ *
+ * Return: Nothing
+ */
+
+void puts2(char *str)
+{
+	int len, i;
+
+	len = strlen(str);
+	for (i = 0; i < len; i += 2)
+		putchar(str[i]);
+	putchar('\n');
+}

47eaefe4cc357cd669c3e6c0c7422574e5a70c51
sermarvin <marv.kariuki@gmail.com>
2023-04-04 05:49:23 +0530

    task 2: strchr function

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string of characters
+ * @c: character in check
+ *
+ * Return: pointer to the character c in check
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s != c)
+		{
+			s++;
+		}
+		else
+		{
+			break;
+		}
+	}
+	return (s);
+}

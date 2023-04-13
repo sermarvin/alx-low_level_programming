7e48b8aee0b0e002d28faef41b96411aa4851b52
sermarvin <marv.kariuki@gmail.com>
2023-04-14 03:18:50 +0530

    task 2: _calloc function

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -5,6 +5,7 @@
  * _calloc - allocates memory for an array, using malloc
  * @nmemb: number of elements
  * @size: number of bytes for each element
+ * The memory is set to 0
  *
  * Return: pointer to the allocated memory
  * NULL if nmemb or size is 0
@@ -13,6 +14,7 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	unsigned int i;
 	void *ptr;
 
 	if (nmemb == 0 || size == 0)
@@ -20,5 +22,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
 		return (NULL);
+	for (i = 0; i < (nmemb * size); i++)
+		*((char *)(ptr) + i) = 0;
 	return (ptr);
 }

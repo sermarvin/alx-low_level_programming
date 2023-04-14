2e1523ca7f12356930f38c0507b8b4f928f01360
sermarvin <marv.kariuki@gmail.com>
2023-04-14 07:54:32 +0530

    task: _realloc function

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -45,7 +45,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		for (i = 0; i < old_size; i++)
 		{
-			*((void **)(newPtr) + i) = *((void **)(ptr) + i);
+			*((char *)(newPtr) + i) = *((char *)(ptr) + i);
 		}
 		free(ptr);
 	}

d2352e9b9e4e8f5ed13d430dc376be7f19a920da
sermarvin <marv.kariuki@gmail.com>
2023-04-18 03:27:17 +0530

    task 3: function-like macro ABS(x)

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,3 +1,8 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define	FUNCTION_LIKE_MACRO_H
+
 #ifndef ABS
-#define ABS(x) ((x) < 0 ? ((x) * (-1)) : ((x) * (1))
+#define ABS(x) ((x) < 0 ? ((x) * (-1)) : ((x) * (1)))
 #endif
+
+#endif /* FUNCTION_LIKE_MACRO_H */

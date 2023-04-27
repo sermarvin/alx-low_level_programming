2e1962756d2e478e9a40f0c92188a7add5f405ff
sermarvin <marv.kariuki@gmail.com>
2023-04-28 03:53:34 +0530

    task 4: frees a list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: head of the list
+ *
+ * Return: 0
+*/
+
+void free_list(list_t *head)
+{
+	list_t *current_node;
+
+	while ((current_node = head) != NULL)
+	{
+		head = head->next;
+		free(current_node->str);
+		free(current_node);
+	}
+}

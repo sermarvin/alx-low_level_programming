9091be647fd89948ece3079fb835115b6d172363
sermarvin <marv.kariuki@gmail.com>
2023-05-01 04:09:42 +0530

    task 5: free function

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list
+ * @head: pointer to the first node
+ *
+ * Return: Nothing
+*/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *curr = *head;
+	listint_t *free_ptr;
+
+	while (curr != NULL)
+	{
+		free_ptr = curr;
+		curr = curr->next;
+		free(free_ptr);
+	}
+	*head = NULL;
+}

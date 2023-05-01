dce1419cd5cc328d054e7830cf0ed772d8f30fd9
sermarvin <marv.kariuki@gmail.com>
2023-05-01 20:20:59 +0530

    task 9: insert node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,11 +19,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
-	new_node->next = NULL;
 	if (*head == NULL)
-	{
 		*head = new_node;
-	}
 	if (idx == 0)
 	{
 		new_node->next = *head;

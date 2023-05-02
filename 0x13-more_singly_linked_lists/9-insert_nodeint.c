2918c38c4c5c0612ca144301f93ecad658ebb88e
sermarvin <marv.kariuki@gmail.com>
2023-05-03 03:55:39 +0530

    task 9: insert node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -41,6 +41,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			counter++;
 			curr = curr->next;
 		}
+		if (idx > counter + 1)
+			return (NULL);
 	}
 	return (new_node);
 }

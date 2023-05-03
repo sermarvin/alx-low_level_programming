32268386e8c8e3ad016d171519b7801689c3240f
sermarvin <marv.kariuki@gmail.com>
2023-05-03 23:09:31 +0530

    advanced task 102: function that fress a linked list

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,30 +1,31 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - prints a linked list, safely
- * @head: list of type listint_t to print
+ * free_listint_safe - function that prints a listint_t linked list
+ * @h: points to the first node in the linked list
  *
- * Return: number of nodes in the list
+ * Return: the number of nodes in the list
  */
-
-size_t print_listint_safe(const listint_t *head)
+size_t free_listint_safe(listint_t **h)
 {
-	size_t num = 0;
-	long int diff;
+	size_t len = 0;
+	listint_t *current, *next;
 
-	while (head)
+	if (!h || !*h)
+		return (0);
+
+	current = *h;
+	while (current)
 	{
-		diff = head - head->next;
-		num++;
-		printf("[%p] %d\n", (void *)head, head->n);
-		if (diff > 0)
-			head = head->next;
-		else
-		{
-			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+		len++;
+		next = current->next;
+		free(current);
+		if (next >= current)
 			break;
-		}
+		current = next;
 	}
 
-	return (num);
+	*h = NULL;
+
+	return (len);
 }

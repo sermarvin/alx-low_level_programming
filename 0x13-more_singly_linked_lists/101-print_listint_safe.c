e6ea8e8fc627c9d9acf57c8d8509c8dd0cef55d4
sermarvin <marv.kariuki@gmail.com>
2023-05-03 22:01:45 +0530

    advanced task 101: print a linked list

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,72 @@
+#include "lists.h"
+
+/**
+ * free_listp - frees a linked list
+ * @head: head of a list
+ *
+ * Return: nothing
+ */
+
+void free_listp(listp_s **head)
+{
+	listp_s *temp;
+	listp_s *curr;
+
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+	free;
+}
+
+/**
+ * print_listint_safe - prints a linked list
+ * @head: head of a list
+ *
+ * Return: Number of nodes in list
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t nnodes = 0;
+	listp_s *hptr, *new, *add;
+
+	hptr = NULL;
+	while (head != NULL)
+	{
+		new = malloc(sizeof(listp_s));
+
+		if (new == NULL)
+			exit(98);
+
+		new->p = (void *)head;
+		new->next = hptr;
+		hptr = new;
+
+		add = hptr;
+
+		while (add->next != NULL)
+		{
+			add = add->next;
+			if (head == add->p)
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free_listp(&hptr);
+				return (nnodes);
+			}
+		}
+
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+		nnodes++;
+	}
+
+	free_listp(&hptr);
+	return (nnodes);
+}

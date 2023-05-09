2e7b6894671cf7e62c925eecf4115ac2329592e6
sermarvin <marv.kariuki@gmail.com>
2023-05-09 23:36:43 +0530

    Updated main.h file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,10 +2,10 @@
 #define _MAIN_H_
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <sys/types.h>
-#include <fnctl.h>
+#include <fcntl.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
